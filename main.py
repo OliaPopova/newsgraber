8af1ab1a42a474941c6a416104805a45b80800a8
Olga2001-Popova <82108597+Olga2001-Popova@users.noreply.github.com>
2022-06-23 08:51:59 +0300

    allworks

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from datetime import date, datetime
 from telethon.tl.functions.messages import GetHistoryRequest
 import pandas as pd
 
+# ссылка https://proglib.io/p/pishem-prostoy-grabber-dlya-telegram-chatov-na-python-2019-11-06
+
 # Считываем учетные данные
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -27,6 +29,18 @@ async def dump_all_messages(channel, url):
         channel_name = 'Научная каторга'
     if url== 'https://t.me/scienpolicy':
         channel_name = 'Научно-образовательная политика'
+    if url=='https://t.me/ivoryzoo':
+        channel_name = 'Зоопарк из слоновой кости'
+    if url=='https://t.me/rasofficial':
+        channel_name = 'Российская академия наук'
+    if url=='https://t.me/RU_Biology':
+        channel_name = 'RU_Biology'
+    # if url == 'https://t.me/+tqDAn2TMFHU0ZWMy':
+    #     channel_name = 'Научная Коммуна'  ссылка бракованная
+    if url == 'https://t.me/minobrnaukiofficial':
+        channel_name = 'Минобрнауки России'
+    # if url == 'https://t.me/interesting_but_true':
+    #     channel_name = 'Высшее образование' канал загнулся
     offset_msg = 0  # номер записи, с которой начинается считывание
     limit_msg = 100  # максимальное число записей, передаваемых за один раз
 
